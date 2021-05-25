bc7cb4d778085d9fd9ee5b155ac423e4d888d960
Dowon Oh <60056004+ohdowon064@users.noreply.github.com>
2021-05-26 03:37:53 +0900

    Database configuration production and development branch handling

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express')
-const config = require("./config.js")
+const config = require("./config/key.js")
 const mongoose = require('mongoose')
+const { User } = require('./models/user.js')
 const app = express()
 const port = 3000
 
-mongoose.connect(config.DATABASE.CONNECT, {
+// application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: true}))
+
+// application/json
+app.use(express.json())
+
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -14,6 +21,20 @@ mongoose.connect(config.DATABASE.CONNECT, {
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+    // 회원가입 데이터를 client에서 가져오면
+    // 해당 데이터들을 데이터베이스에 넣는다.
+
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+
 })
 
 app.listen(port, () => {
