5015c2400bc6075878d91ce581386690cb74d378
Dowon Oh <60056004+ohdowon064@users.noreply.github.com>
2021-05-25 23:59:12 +0900

    connect mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const database = require("./config.js").DATABASE
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
+
+mongoose.connect(database.CONNECT, {
+    userNewUrlPaser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => console.log("MongoDB Connected..."))
+  .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
