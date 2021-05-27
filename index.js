9a339101bbc3b07818a8c0c03d21fca95c6ba294
Dowon Oh <60056004+ohdowon064@users.noreply.github.com>
2021-05-28 01:47:14 +0900

    login api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const config = require("./config/key.js")
 const mongoose = require('mongoose')
 const { User } = require('./models/user.js')
+const cookieParser = require('cookie-parser')
 const app = express()
 const port = 3000
 
@@ -28,6 +29,7 @@ app.post('/register', (req, res) => {
     // 해당 데이터들을 데이터베이스에 넣는다.
 
     const user = new User(req.body)
+
     user.save((err, userInfo) => {
         if(err) return res.json({success: false, err})
         return res.status(200).json({
@@ -35,6 +37,41 @@ app.post('/register', (req, res) => {
         })
     })
 
+})
+
+app.post('/login', (req, res) => {
+  
+  // 요청된 이메일을 DB에서 찾는다.
+  User.findOne({email: req.body.email}, (err, user) => {
+    if(!user) {
+      return res.json({
+        loginSuccess: false,
+        message : "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+
+    // 비밀번호 비교
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch)
+        return res.json({
+          loginSuccess: false, 
+          message: "비밀번호가 틀렸습니다."
+        })
+      
+      // 토큰 생성
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err)
+
+        // 토큰을 어디에 저장? 쿠키, 세션, 로컬스토리지
+        res.cookie("x_auth", user.token)
+          .status(200)
+          .json({
+            loginSuccess: true,
+            userId: user._id
+          })
+      })
+    })
+  })
 })
 
 app.listen(port, () => {
--- models/user.js
+++ models/user.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const config = require('../config/key.js')
+
+const saltRounds = 10
 
 const userSchema = mongoose.Schema({
     name: {
@@ -9,6 +14,9 @@ const userSchema = mongoose.Schema({
         type: String,
         trim: true,
         minlength: 5
+    },
+    password : {
+        type: String
     },
     lastname: {
         type: String,
@@ -26,6 +34,48 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+// 저장 전 동작정의
+userSchema.pre('save', function(next) {
+    
+    var user = this
+
+    if(user.isModified('password')) {
+
+        // 비밀번호 암호화
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err)
+
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+
+                user.password = hash
+                next()
+            })
+        })
+    } else {
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, callback) {
+    bcrypt.compare(plainPassword, this.password, (err, isMatch) => {
+        if(err) return callback(err)
+        callback(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(callback) {
+    var user = this;
+
+    // jsonwebtoken을 사용해서 token 생성
+    var token = jwt.sign(user._id.toHexString(), config.secretKey)
+    user.token = token
+    user.save((err, user) => {
+        if(err) return callback(err)
+        callback(null, user)
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 module.exports = {User}
